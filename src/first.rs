60bebb07e1912a2e01df5d936bc321625b7c59a7
Naoto Takezawa <naoto9march@gmail.com>
2020-03-22 16:40:08 +0900

    要素が0, 1個の場合のテストを追加

--- src/first.rs
+++ src/first.rs
@@ -60,6 +60,7 @@ mod tests {
         assert_eq!(array, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
+    //　降順ソートのテスト
     #[test]
     fn sort_u32_descending() {
         let mut array = vec![10, 30 , 11, 20, 4, 330, 21, 110];
@@ -67,5 +68,45 @@ mod tests {
         sort(&mut array, false);
 
         assert_eq!(array, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    // 要素が0個（昇順）
+    #[test]
+    fn sort_ascending_zero() {
+        let mut array = vec![];
+        
+        sort(&mut array, true);
+
+        assert_eq!(array, vec![]);
+    }
+
+    // 要素が0個（降順）
+    #[test]
+    fn sort_descending_zero() {
+        let mut array = vec![];
+        
+        sort(&mut array, false);
+
+        assert_eq!(array, vec![]);
+    }
+
+    // 要素が1個（昇順）
+    #[test]
+    fn sort_ascending_one() {
+        let mut array = vec![10];
+        
+        sort(&mut array, true);
+
+        assert_eq!(array, vec![10]);
+    }
+
+    // 要素が1個（降順）
+    #[test]
+    fn sort_descending_one() {
+        let mut array = vec![45];
+        
+        sort(&mut array, false);
+
+        assert_eq!(array, vec![45]);
     }
 }
