7530717bb3ffd6493165dff6305b5b73237ecaba
Naoto Takezawa <naoto9march@gmail.com>
2020-03-28 14:25:22 +0900

    ソート順をenumで置き換えた

--- src/second.rs
+++ src/second.rs
@@ -1,47 +1,52 @@
+use crate::SortOrder;
+use crate::SortOrder::*;
+
 // 配列をソートする
 // array : 配列
-// asc : 昇順の場合true、降順の場合false
-pub fn sort<T: Ord>(array: &mut[T], asc: bool) {
+// order : ソート順
+pub fn sort<T: Ord>(array: &mut[T], order: &SortOrder) {
     if array.len() <= 1 {
         return;
     }
 
     let mid = array.len() / 2;
-    sort(&mut array[..mid], true); // 前半を昇順
-    sort(&mut array[mid..], false); // 後半を降順でソート
-    sub_sort(array, asc);
+    sort(&mut array[..mid], &Ascending); // 前半を昇順
+    sort(&mut array[mid..], &Descending); // 後半を降順でソート
+    sub_sort(array, order);
 }
 
 // バイトニック数列をソートする
 // array : バイトニック配列
-// asc : 昇順の場合true、降順の場合false
-fn sub_sort<T: Ord>(array: &mut[T], asc: bool) {
+// order : ソート順
+fn sub_sort<T: Ord>(array: &mut[T], order: &SortOrder) {
     if array.len() <= 1 {
         return;
     }
-    // 比較＆入れ替えによって並び順をascに近づける（ソートは不完全）
-    compare_and_swap(array, asc);
+    // 比較＆入れ替えによって並び順をorderに近づける（ソートは不完全）
+    compare_and_swap(array, order);
     let mid = array.len() / 2;
-    sub_sort(&mut array[..mid], asc);
-    sub_sort(&mut array[mid..], asc);
+    sub_sort(&mut array[..mid], order);
+    sub_sort(&mut array[mid..], order);
 }
 
 // 昇順か降順かに応じて前半と後半がそれぞれバイトニック数列になるように要素を並べ替える
-fn compare_and_swap<T: Ord>(array: &mut[T], asc: bool){
+fn compare_and_swap<T: Ord>(array: &mut[T], order: &SortOrder){
     let mid = array.len() / 2;
 
     for i in 0..mid {
-        if asc {
-            //　昇順の場合
-            if array[i] > array[i + mid] {
-                array.swap(i, i + mid);
-            }
-        }
-        else {
-            // 降順の場合
-            if array[i] < array[i + mid] {
-                array.swap(i, i + mid);
-            }
+		match *order {
+			Ascending => {
+				//　昇順の場合
+				if array[i] > array[i + mid] {
+					array.swap(i, i + mid);
+				}
+			},
+			Descending => {
+				// 降順の場合
+				if array[i] < array[i + mid] {
+					array.swap(i, i + mid);
+				}
+			},
         }
     }
 }
@@ -49,13 +54,14 @@ fn compare_and_swap<T: Ord>(array: &mut[T], asc: bool){
 #[cfg(test)]
 mod tests {
     use super::sort;
+	use crate::SortOrder::*;
 
     // 昇順ソートのテスト
     #[test]
     fn sort_u32_ascending() {
         let mut array: Vec<u32> = vec![10, 30 , 11, 20, 4, 330, 21, 110];
         
-        sort(&mut array, true);
+        sort(&mut array, &Ascending);
 
         assert_eq!(array, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
@@ -65,7 +71,7 @@ mod tests {
     fn sort_u32_descending() {
         let mut array: Vec<u32> = vec![10, 30 , 11, 20, 4, 330, 21, 110];
         
-        sort(&mut array, false);
+        sort(&mut array, &Descending);
 
         assert_eq!(array, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
@@ -75,7 +81,7 @@ mod tests {
     fn sort_ascending_zero() {
         let mut array: Vec<u32> = vec![];
         
-        sort(&mut array, true);
+        sort(&mut array, &Ascending);
 
         assert_eq!(array, vec![]);
     }
@@ -85,7 +91,7 @@ mod tests {
     fn sort_descending_zero() {
         let mut array: Vec<u32> = vec![];
         
-        sort(&mut array, false);
+        sort(&mut array, &Descending);
 
         assert_eq!(array, vec![]);
     }
@@ -95,7 +101,7 @@ mod tests {
     fn sort_ascending_one() {
         let mut array: Vec<u32> = vec![10];
         
-        sort(&mut array, true);
+        sort(&mut array, &Ascending);
 
         assert_eq!(array, vec![10]);
     }
@@ -105,7 +111,7 @@ mod tests {
     fn sort_descending_one() {
         let mut array: Vec<u32> = vec![45];
         
-        sort(&mut array, false);
+        sort(&mut array, &Descending);
 
         assert_eq!(array, vec![45]);
     }
@@ -115,7 +121,7 @@ mod tests {
     fn sort_str_ascending() {
         let mut array = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
         
-        sort(&mut array, true);
+        sort(&mut array, &Ascending);
 
         assert_eq!(array, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
     }
@@ -125,7 +131,7 @@ mod tests {
     fn sort_str_descending() {
         let mut array = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
         
-        sort(&mut array, false);
+        sort(&mut array, &Descending);
 
         assert_eq!(array, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"]);
     }
